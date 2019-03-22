b988c8be551aeb8b6ba03412a5ee865f8f4b28f6
Bastian Ilsø <bastianilso@gnome.org>
2019-03-22 22:27:30 +0100

    HidingScene: Implement Button which switches to Seeking Scene

---
+++ HjemmeSowwy/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private GameManager instance;
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void StartSeekingGame() {
+        SceneManager.LoadSceneAsync("SeekingScene");
+    }
+}
