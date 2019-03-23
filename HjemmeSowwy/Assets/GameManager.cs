da3819f0a566c5b45a4ad28909c6a2df3b39f360
Bastian Ilsø <bastianilso@gnome.org>
2019-03-23 22:18:49 +0100

    Add HUD to SeekerScene

--- HjemmeSowwy/Assets/GameManager.cs
+++ HjemmeSowwy/Assets/GameManager.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public struct Clue {
     public string clueText;
     public bool read;
 }
 
+public struct TotalScore {
+    public int seekerScore;
+    public int hiderScore;
+    public int rounds;
+}
+    public enum SeekerState {
+        Standby,
+        Countdown,
+        Seeking,
+        GameOver
+
+    }
 public class GameManager : MonoBehaviour
 {
+
+
+
     public string hidingObject;
     public static GameObject instance;
     public List<Clue> clues;
+    private int cluesLeft = -1;
+    public TotalScore totalScore;
+
+    public float seekTimeMax = 180;
+    public float remainingTime = -1;
+    public SeekerState seekerState;
+
+    public int currentSeekerScore = 0;
+    public int currentHiderScore = 0;
+    public int seekerCountDown = 5;
 
     void Awake() {
         if (instance == null) {
@@ -25,12 +51,34 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         clues = new List<Clue>();
+        totalScore = new TotalScore();
+        totalScore.seekerScore = 0;
+        totalScore.hiderScore = 0;
+        totalScore.rounds = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (seekerState == SeekerState.Standby) {
+            // do nothing
+        } else if (seekerState == SeekerState.Countdown) {
+            // count down
+        } else if (seekerState == SeekerState.Seeking) {
+            if (remainingTime > 0f) {
+                remainingTime -= Time.deltaTime;
+                currentSeekerScore = Mathf.RoundToInt(remainingTime*100);
+                currentHiderScore = Mathf.RoundToInt((seekTimeMax - remainingTime)*100);
+            } else {
+                seekerState = SeekerState.GameOver;
+            }
+        } else if (seekerState == SeekerState.GameOver) {
+            // Trigger High score table
+            // Calculate number of clues read
+            // Award points based on that
+            // Calculate new TotalScore
+            seekerState = SeekerState.Standby;
+        }
     }
 
     public void SetHidingObject(string id) {
@@ -52,10 +100,32 @@ public class GameManager : MonoBehaviour
                 Clue updatedClue = new Clue();
                 updatedClue.clueText = clues[i].clueText;
                 updatedClue.read = true;
+                cluesLeft--;
                 clues[i] = updatedClue;
             }
         }
         return nextClue;
+    }
+
+    public int[] GetClueStats() {
+        int[] cluestats = new int[2];
+        cluestats[0] = cluesLeft;
+        cluestats[1] = clues.Count;
+        return cluestats;
+    }
+
+    public SeekerState GetSeekerState() {
+        return seekerState;
+    }
+
+    public void PrepareSeekGame() {
+        cluesLeft = clues.Count;
+        remainingTime = seekTimeMax;
+        seekerState = SeekerState.Countdown;
+    }
+
+    public void StartSeekGame() {
+        seekerState = SeekerState.Seeking;
     }
 
 }
---
+++ HjemmeSowwy/Assets/UIOverlayManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOverlayManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text timeText;
+
+    private string timeTextTemplate;
+
+    [SerializeField]
+    private Text clueText;
+
+    private string clueTextTemplate;
+    
+    [SerializeField]
+    private Text countDownText;
+    private float currentCountDown;
+
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Managers").GetComponent<GameManager>();
+        timeTextTemplate = timeText.text;
+        clueTextTemplate = clueText.text;
+        currentCountDown = (float) gameManager.seekerCountDown;
+        gameManager.PrepareSeekGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cluestats = gameManager.GetClueStats();
+        timeText.text = string.Format(timeTextTemplate, gameManager.remainingTime);
+        clueText.text = string.Format(clueTextTemplate, cluestats[0], cluestats[1]);
+        if (gameManager.seekerState == SeekerState.Countdown) {
+            currentCountDown -= Time.deltaTime;
+            if (currentCountDown < 1) {
+                countDownText.gameObject.SetActive(false);
+                gameManager.StartSeekGame();
+            } else {
+                countDownText.gameObject.SetActive(true);
+                countDownText.text = Mathf.Round(currentCountDown).ToString();
+            }
+        }
+    }
+}
